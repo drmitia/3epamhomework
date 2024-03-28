2f4baa1381ecf8a4c13bd0710c6e53d62eaa2503
drmitia <drdrmitia@gmail.com>
2024-03-28 14:57:27 +0200

    3epam

---
+++ 3epanhomework/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _3epanhomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть ваше число");
+            int our_number = Convert.ToInt32(Console.ReadLine());
+            string numberString = our_number.ToString();
+            int length = numberString.Length;
+            int summa = 0;
+            while (length > 0)
+            {
+                int k = our_number % 10;
+                our_number /= 10;
+                summa =summa + k;
+                length--;               
+            }
+            Console.WriteLine(summa);
+        }
+    }
+}
---
+++ ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть число А");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введiть число В(менше за А)");
+            int B = Convert.ToInt32(Console.ReadLine());
+            while (A >= B) 
+            {
+                Console.WriteLine(A);
+                A--;
+            }
+        }
+    }
+}
